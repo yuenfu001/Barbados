b1256022e8e77a909a38afd40e9ab7929110a143
yuenfu001 <liuyuenfu@gmail.com>
2024-10-09 22:16:37 +0100

    adjusted contact space

--- barbados/settings.py
+++ barbados/settings.py
@@ -26,12 +26,12 @@ SECRET_KEY = config("FUNC")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG")
 
-<<<<<<< HEAD
+
 ALLOWED_HOSTS = ["*",".vercel.app"]
 # ALLOWED_HOSTS = ["localhost", "192.168.137.1", "127.0.0.1","192.168.1.140","barbados.com","10.0.0.125"]
-=======
+
 ALLOWED_HOSTS = ["localhost", "192.168.137.1", "127.0.0.1","192.168.1.140","barbados.com","10.0.0.125",".vercel.app"]
->>>>>>> 6235154fcbf8b3c0b1ff94faeaab022ca4c45110
+
 DEFAULT_FILE_STORAGE = "django.core.files.storage.FileSystemStorage"  # use the apprropriate storage for backend here
 
 # Application definition
